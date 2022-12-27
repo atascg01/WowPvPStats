cd5912b7a550f61adfc75733a276f85d252df6bd
atascg01 <atascg01@estudiantes.unileon.es>
2022-12-27 19:36:52 +0100

    Rankings implementation

--- src/components/Rankings.jsx
+++ src/components/Rankings.jsx
@@ -1,4 +1,33 @@
 import React from 'react';
+import axios from 'axios';
+import { CLIENT_ID, CLIENT_SECRET } from '../secrets';
+
+async function getAccessToken() {
+  const response = await axios.post('https://us.battle.net/oauth/token', {
+    grant_type: 'client_credentials',
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+  });
+
+  return response.data.access_token;
+}
+
+async function getUserData(accessToken) {
+  const response = await axios.get('https://us.api.blizzard.com/wow/user/characters', {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  return response.data;
+}
+
+async function handleLogin() {
+  const accessToken = await getAccessToken();
+  const userData = await getUserData(accessToken);
+
+  // Use the user data in your application
+}
 
 function Rankings() {
     return (
